d00299a7c75b8f77a2d9a0bb79bf576b51ad2b43
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-05-15 14:44:58 +0000

    Added new line in settings.py

--- drf_api_backend/settings.py
+++ drf_api_backend/settings.py
@@ -73,7 +73,7 @@ DEBUG = "DEV" in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get("ALLOWED_HOST"),
-    'localhost',
+    'localhost', "127.0.0.1",
 ]
 
 
@@ -119,15 +119,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
-
 
 CORS_ALLOW_CREDENTIALS = True
 
