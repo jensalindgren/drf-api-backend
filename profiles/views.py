29246578023cd72f22d948ba2ad58eebe176b25c
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-05-15 10:18:08 +0000

    Now I can view profile list

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from profiles.models import Profile
+from rest_framework import generics, permissions
+from profiles.serializers import ProfileSerializer
+from drf_api_backend.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class ProfileList(generics.ListCreateAPIView):
+    """
+    List all profiles or create a new profile
+    """
+    queryset = Profile.objects.all().order_by("-created_at")
+    serializer_class = ProfileSerializer
+
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a profile instance
+    """
+    queryset = Profile.objects.all().order_by("-created_at")
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
