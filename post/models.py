e5f53054993c107cc4b4013469bbe68d4fbad694
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-05-17 14:00:01 +0000

    cleaning up code and adding comments

--- post/models.py
+++ post/models.py
@@ -7,22 +7,6 @@ class Post(models.Model):
     Post model to be created by staff
     Default image set so that we can always reference image.url.
     """
-    image_filter_choices = [
-        ('_1977', '1977'),
-        ('brannan', 'Brannan'),
-        ('earlybird', 'Earlybird'),
-        ('hudson', 'Hudson'),
-        ('inkwell', 'Inkwell'),
-        ('lofi', 'Lo-Fi'),
-        ('kelvin', 'Kelvin'),
-        ('normal', 'Normal'),
-        ('nashville', 'Nashville'),
-        ('rise', 'Rise'),
-        ('toaster', 'Toaster'),
-        ('valencia', 'Valencia'),
-        ('walden', 'Walden'),
-        ('xpro2', 'X-pro II')
-    ]
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -35,9 +19,6 @@ class Post(models.Model):
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to="images/", default='../default_post_sqpxy8', blank=True,)
-    image_filter = models.CharField(
-        max_length=32, choices=image_filter_choices, default='normal'
-    )
 
     class Meta:
         ordering = ["-created_at"]
