5d3ea4a3695a1b5e3090faa74e0403ac240486fc
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-05-27 19:14:32 +0000

    wrong url

--- drf_api_backend/urls.py
+++ drf_api_backend/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
     path('', include('profiles.urls')),
-    path('', include('posts.urls')),
+    path('', include('post.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
