e6d1a4dd20af26a3544b7eed396a7f5f9599c0d7
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-05-27 18:33:41 +0000

    Adding more comments to the files. Updating the files. New views added. Settings and serializers updated.

--- drf_api_backend/serializers.py
+++ drf_api_backend/serializers.py
@@ -11,6 +11,16 @@ class CurrentUserSerializer(UserDetailsSerializer):
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
     is_staff = serializers.ReadOnlyField(source="profile.is_staff")
 
+    def get_profile_image(self, obj):
+        """
+        Serializer method for profile image, to enable
+        implementing a fix for cloudinary not serving images securely
+        """
+        # Fix for cloudinary not serving images securely is from
+        # https://stackoverflow.com/questions/48508750/how-to-force-https-in-a-django-project-using-cloudinary
+        obj.profile.image.url_options.update({'secure': True})
+        return obj.profile.image.url
+
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             'profile_id',
