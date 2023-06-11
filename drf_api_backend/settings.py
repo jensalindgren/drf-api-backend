bec3123d263f72b208832d5c045f69187fa84f7a
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-06-11 13:18:47 +0000

    Still having problem with my following button on the frontend.

--- drf_api_backend/settings.py
+++ drf_api_backend/settings.py
@@ -44,7 +44,7 @@ REST_FRAMEWORK = {
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 999999999,
+    'PAGE_SIZE': 100,
     "DATETIME_FORMAT": "%d %b %Y",
 }
 
