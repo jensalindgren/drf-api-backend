6ddb7cd768d3070c45c35a03788b6980198d13be
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-06-12 19:08:44 +0000

    just a small change to staff model

--- profiles/models.py
+++ profiles/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
+    is_staff = models.BooleanField(default=False)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_j1uwjo')
 
