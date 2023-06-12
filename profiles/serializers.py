bb37a53007baf72ae7191708d798c925f1c43c6f
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-06-12 19:08:30 +0000

    Just a small change to staff model

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -10,7 +10,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     posts_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
-    is_staff = serializers.ReadOnlyField(source='owner.is_staff')
+    is_staff = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
